40d26775d7dac48e71b6df39ac02e85db9bcc3d5
BWC4WIFE <114278909+BWC4WIFE@users.noreply.github.com>
2025-07-18 00:56:57 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.code.gson:gson:2.10.1")
 
+// For DataBinding
+    kapt "com.android.databinding:compiler:8.2.0"
+
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
