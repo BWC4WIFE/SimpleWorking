01259d0ff80ff8e64d822564f129868751066fef
BWC4WIFE <114278909+BWC4WIFE@users.noreply.github.com>
2025-07-17 19:03:10 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+ plugins {
+        id("com.android.application") version "8.2.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+        id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
@@ -14,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "GemWebLive"
+rootProject.name = "BWCTrans"
 include(":app")
