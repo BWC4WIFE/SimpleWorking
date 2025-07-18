5701367685503a67b7ee3a989d99c3f5328a1cd0
BWC4WIFE <114278909+BWC4WIFE@users.noreply.github.com>
2025-07-18 15:35:07 +0700

    Update UserSettingsDialogFragment.kt

--- app/src/main/java/com/UserSettingsDialogFragment.kt
+++ app/src/main/java/com/UserSettingsDialogFragment.kt
@@ -10,7 +10,6 @@ import android.app.Dialog
 import android.widget.FrameLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.bwctrans.databinding.DialogUserSettingsBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior// IMPORTANT: Use the new binding class
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import android.Manifest
 import androidx.core.content.ContextCompat
