e55ed45d3b46b5e41fac7b34ce906939bfd55d19
BWC4WIFE <114278909+BWC4WIFE@users.noreply.github.com>
2025-07-18 09:13:48 +0700

    Update SettingsDialog.kt

--- app/src/main/java/com/SettingsDialog.kt
+++ app/src/main/java/com/SettingsDialog.kt
@@ -11,7 +11,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.SeekBar
-import com.Bwctrans.databinding.DialogSettingsBinding
+import com.bwctrans.databinding.DialogSettingsBinding
 
 // --- CLASS & INTERFACE ---
 class SettingsDialog(
